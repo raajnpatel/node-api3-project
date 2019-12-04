aa50750a853d34e851cacb49a54bb6f7abf403dc
rndpatel@gmail.com <rndpatel@gmail.com>
2019-12-04 10:57:10 -0800

    GET/PUT by ID

--- users/userRouter.js
+++ users/userRouter.js
@@ -8,7 +8,7 @@ router.post('/', (req, res) => {
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
 });
 
 router.get('/', (req, res) => {
@@ -27,19 +27,60 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+    User.getById(id)
+        .then(user => {
+            if(user) {
+                res
+                    .status(200)
+                    .json(user);
+            } else {
+                res
+                    .status(404)
+                    .json({error: "User ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"There was a problem reaching the server."})
+        })
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+    const { id } = req.params;
+    const { name } = req.body;
+    User.getById(id)
+        .then(user => {
+            if(user){
+                User.update(id, {name})
+                    .then(updated => {
+
+                        res
+                            .status(200)
+                            .json(updated)
+                    })
+            } else {
+                res
+                    .status(404)
+                    .json({error:"User ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"There was a problem reaching the server."})
+        })
 });
 
 //custom middleware
